2662db6e3a5d0556609c62383a7bf46ae55d8cb9
Amankumar Churiwal <amanchuriwal22@gmail.com>
2023-07-19 14:04:40 +0530

    Updates

---
+++ precache-manifest.94a599c68d9fedee141286f1bd11a927.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2549c173ea4dd2e12af59770dd8de0",
+    "url": "/note-web-app/index.html"
+  },
+  {
+    "revision": "b1b29cf9efb7207dbe7d",
+    "url": "/note-web-app/static/js/2.df0d89d3.chunk.js"
+  },
+  {
+    "revision": "d5b8335acf357426d7b0",
+    "url": "/note-web-app/static/js/main.76e5c4ef.chunk.js"
+  },
+  {
+    "revision": "c3718a89f4237a171ffc",
+    "url": "/note-web-app/static/js/runtime-main.f26de704.js"
+  }
+]);
